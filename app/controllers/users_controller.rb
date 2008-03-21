29142971a1bc2cc4e9a743bbff5fed05ba85addf
Jens-Christian Fischer <jcf@invisible.ch>
2008-03-21 12:35:06 +0100

    * simple YAML based configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  skip_before_filter :login_required, :only => [:create, :activate]
+  skip_before_filter :login_required, :only => [:new, :create, :activate]
   
   # Protect these actions behind an admin login
   # before_filter :admin_required, :only => [:suspend, :unsuspend, :destroy, :purge]
--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,23 +2,24 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Please activate your new account'
-  
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
-  
+
+    @body[:url]  = "http://#{APPLICATION['site_host']}/activate/#{user.activation_code}"
+
   end
-  
+
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://#{APPLICATION['site_host']}/"
   end
-  
+
   protected
-    def setup_email(user)
-      @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
-      @sent_on     = Time.now
-      @body[:user] = user
-    end
+  def setup_email(user)
+    @recipients  = "#{user.email}"
+    @from        = APPLICATION['admin_email']
+    @subject     = "[#{APPLICATION['app_name']}] "
+    @sent_on     = Time.now
+    @body[:user] = user
+  end
 end
+
