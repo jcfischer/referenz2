2635e5b0dd1718e8fcd591b823917d12d4589ea5
Jens-Christian Fischer <jcf@invisible.ch>
2008-04-08 11:01:21 +0200

    * search form * setting mongrel user

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,12 @@ class SearchController < ApplicationController
 
   def index
     @query = params[:search]
-    @search = Ultrasphinx::Search.new(:query => @query, :per_page => 2, :page => params[:page] || 1)
+    sort_mode = params[:search_sort] || 'relevance'
+    sort_by = sort_mode == 'relevance' ? nil : 'created_at'
+    model = params[:search_model] || nil
+    @search = Ultrasphinx::Search.new(:query => @query, :per_page => 2, :page => params[:page] || 1,
+                                      :sort_mode => sort_mode,:sort_by => sort_by, 
+                                      :class_name => model)
     @search.excerpt
     @results = @search.results
   end
