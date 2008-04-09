8c0ad0d6ab4fb09b475b9eb9c14502fc1f0fe304
Jens-Christian Fischer <jcf@invisible.ch>
2008-04-09 17:23:30 +0200

    ultrasphinx rake tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,3 +72,34 @@ namespace :deploy do
 
   end
 end
+
+
+# ==========================
+# Sphinx Server Tasks
+# ==========================
+namespace :ultrasphinx do
+  
+  desc "Create Config File"
+  task :config, :roles => :app do
+    invoke_command "cd #{current_path} && rake ultrasphinx:configure RAILS_ENV=production"
+  end
+  desc "Update Index"
+  task :index, :roles => :app do
+    invoke_command "cd #{current_path} && rake ultrasphinx:index RAILS_ENV=production"
+  end
+
+  desc "Stop Sphinx"
+  task :daemon_stop, :roles => :app do
+    invoke_command "cd #{current_path} && rake ultrasphinx:daemon:stop RAILS_ENV=production"
+  end
+
+  desc "Start Sphinx"
+  task :daemon_start, :roles => :app do
+    invoke_command "cd #{current_path} && rake ultrasphinx:daemon:start RAILS_ENV=production"
+  end
+
+  desc "Restart Sphinx"
+  task :daemon_restart, :roles => :app do
+    invoke_command "cd #{current_path} && rake ultrasphinx:daemon:restart RAILS_ENV=production"
+  end
+end
