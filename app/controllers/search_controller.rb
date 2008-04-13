74be7bbbe1668d4552dac311e610d158a5e380c1
Jens-Christian Fischer <jcf@invisible.ch>
2008-04-13 10:56:16 +0200

    * fixed search controller spec

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,7 @@ class SearchController < ApplicationController
     sort_mode = params[:search_sort] || 'relevance'
     sort_by = sort_mode == 'relevance' ? nil : 'created_at'
     model = params[:search_model] || nil
-    @search = Ultrasphinx::Search.new(:query => @query, :per_page => 2, :page => params[:page] || 1,
+    @search = Ultrasphinx::Search.new(:query => @query, :per_page => 10, :page => params[:page] || 1,
                                       :sort_mode => sort_mode,:sort_by => sort_by, 
                                       :class_name => model)
     @search.excerpt
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -20,7 +20,7 @@ describe SearchController do
     end
     
     it "should call the Ultraspinx search" do
-      Ultrasphinx::Search.should_receive(:new).with(:query => "suche", :per_page => 2, :page => 1).and_return(@search)
+      Ultrasphinx::Search.should_receive(:new).with(:query => "suche", :per_page => 10, :page => 1, :sort_mode => 'relevance', :sort_by => nil, :class_name => nil).and_return(@search)
       do_get
     end
     
