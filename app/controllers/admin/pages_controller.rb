c273e352dd50d607b693f2b64b17726e37e9a655
Jens-Christian Fischer <jcf@invisible.ch>
2008-04-27 00:01:18 +0200

    * pages/users admin

---
+++ app/controllers/admin/pages_controller.rb
@@ -0,0 +1,29 @@
+class Admin::PagesController < ApplicationController
+
+
+  layout 'referenz'
+  make_resourceful do
+    actions :index, :show, :destroy
+
+  end
+
+  def delete_many
+    ids = params[:page]
+    ids.each do |id|
+      begin
+        Page.delete id
+        # Comment.destroy id
+      rescue
+        # comment was already destroyed through dependecy
+      end
+    end
+    redirect_to admin_pages_path
+  end
+
+  protected
+  def current_objects
+    Page.paginate(:all, :order => 'title', :page => params[:page], :per_page => 15)
+  end
+
+
+end
---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,27 @@
+class Admin::UsersController < ApplicationController
+  
+  
+  layout 'referenz'
+  make_resourceful do
+    actions :index, :show, :destroy
+
+  end
+
+  def delete_many
+    ids = params[:page]
+    ids.each do |id|
+      begin
+        User.delete id
+        # Comment.destroy id
+      rescue
+        # comment was already destroyed through dependecy
+      end
+    end
+    redirect_to admin_users_path
+  end
+
+  protected
+  def current_objects
+    User.paginate(:all, :order => 'login', :page => params[:page], :per_page => 15)
+  end
+end
