5b8a015e612398df656ea75fc468a133909ab10e
Jens-Christian Fischer <jcf@invisible.ch>
2008-05-03 17:53:58 +0200

    forgot environment

--- config/environment.rb
+++ config/environment.rb
@@ -17,6 +17,23 @@ end
 
 ENV['HOME'] = '/home/praxis' if ENV['RAILS_ENV'] == 'production'
 
+# Authorization plugin for role based access control
+# You can override default authorization system constants here.
+
+# Can be 'object roles' or 'hardwired'
+AUTHORIZATION_MIXIN = "object roles"
+# NOTE : If you use modular controllers like '/admin/products' be sure 
+# to redirect to something like '/sessions' controller (with a leading slash)
+# as shown in the example below or you will not get redirected properly
+#
+# This can be set to a hash or to an explicit path like '/login'
+#
+LOGIN_REQUIRED_REDIRECTION = { :controller => '/sessions', :action => 'new' }
+PERMISSION_DENIED_REDIRECTION = { :controller => '/home', :action => 'index' }
+# The method your auth scheme uses to store the location to redirect back to 
+STORE_LOCATION_METHOD = :store_location
+
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
 
