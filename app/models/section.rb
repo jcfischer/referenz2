19b0e068d9251c46f463c6ee77e3b3d3b616b80f
Jens-Christian Fischer <jcf@invisible.ch>
2008-11-26 18:22:12 +0100

    ! removing ft for book for now

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,9 +3,9 @@ class Section < ActiveRecord::Base
   belongs_to :chapter
   use_permalink :title
   
-  is_indexed :fields => [ 'created_at', 'title', 'body',
-                          {:field => 'title', :sortable => true}],
-              :delta => true
+  #is_indexed :fields => [ 'created_at', 'title', 'body',
+  #                        {:field => 'title', :sortable => true}],
+  #            :delta => true
 
   def ordinal
     number ? "#{number}." : number
