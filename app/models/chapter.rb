e7cd8fd109597674d7e05b71dc72c8c60acd546b
Jens-Christian Fischer <jcf@Arwen.local>
2008-11-26 18:12:16 +0100

    + ft indexing of book

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,6 +2,10 @@ class Chapter < ActiveRecord::Base
   
   has_many :sections
   use_permalink :title
+
+  is_indexed :fields => [ 'created_at', 'title', 'body',
+                          {:field => 'title', :sortable => true}],
+              :delta => true
   
   def ordinal
     number ? "#{number}." : number
